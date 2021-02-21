109210c854ef00023cd5df6c63754b0b0f3361f7
shunCode <61289925+shunCode@users.noreply.github.com>
2021-02-22 06:08:22 +0900

    Level1-완주하지 못한 선수

---
+++ Level1/완주하지 못한 선수.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+
+    string answer = "";
+    unordered_map<string, int> hmap;
+
+    for (string name : completion)
+    {
+        hmap[name]++;  // 마라톤 완주자들을 등록
+    }
+
+    for (string name : participant)
+    {
+        if (hmap[name])  // 완주자 명단에 있는 경우
+        {
+            hmap[name]--;
+        }
+        else  // 완주자 명단에 없는 경우
+        {
+            answer = name;
+            break;
+        }
+    }
+
+    return answer;
+}

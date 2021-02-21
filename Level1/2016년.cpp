42a5215dbca69d1114ab091d2aa47bbb4c5a2e85
shunCode <61289925+shunCode@users.noreply.github.com>
2021-02-22 07:44:43 +0900

    Level1-2016년

---
+++ Level1/2016년.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+
+    string answer = "";
+    int day_diff = 0;
+
+    vector<string> week = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+    vector<int> day = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    for (int i = 0; i < a - 1; i++) day_diff += day[i];  // 월 차이 계산
+    day_diff += b - 1;  // 일 차이 계산
+
+    answer = week[(day_diff + 5) % 7];
+
+    return answer;
+}

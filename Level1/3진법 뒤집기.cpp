28d324f1d8798dc266a3d20344bf427af4f4ed19
shunCode <k_shun@naver.com>
2021-02-22 15:55:47 +0900

    Level1-3진법 뒤집기

---
+++ Level1/3진법 뒤집기.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n) {
+
+    int answer = 0;
+    vector<int> vect;
+
+    // 3진법으로 변환 & 앞뒤 반전
+    while (n)
+    {
+        vect.push_back(n % 3);
+        n /= 3;
+    }
+
+    // 10진법으로 표현
+    for (int i = 0; i < vect.size(); i++)
+        answer += vect[i] * pow(3, vect.size() - i - 1);
+
+    return answer;
+}

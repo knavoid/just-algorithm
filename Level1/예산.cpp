5405260c9bae8b4346698e4d69723cd75454baa4
shunCode <61289925+shunCode@users.noreply.github.com>
2021-02-25 06:36:06 +0900

    Level1-예산

---
+++ Level1/예산.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    
+    int answer = 0;
+
+    sort(d.begin(), d.end());
+    for (int i : d)
+    {
+        if (i <= budget)
+        {
+            budget -= i;
+            answer++;
+        }
+        else break;
+    }
+
+    return answer;
+}

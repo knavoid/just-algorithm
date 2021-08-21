edd9cf08e24b8c041dda383a037a7a08290cd74e
shunCode <61289925+shunCode@users.noreply.github.com>
2021-08-21 18:02:03 +0900

    Update H-Index.cpp

--- Level2/H-Index.cpp
+++ Level2/H-Index.cpp
@@ -6,23 +6,10 @@ using namespace std;
 
 int solution(vector<int> citations) {
 
-    int h_index = 0;
-
     sort(citations.begin(), citations.end());
 
-    for (int h = 1; h <= citations.size(); h++)
-    {
-        int inyong = 0;
-        for (int i = 0; i < citations.size(); i++)
-        {
-            if (citations.at(i) >= h)
-            {
-                inyong = citations.size() - i;
-                break;
-            }
-        }
-        if (inyong >= h) h_index = h;
-    }
+    for (int h = 0; h <= citations.size(); h++)
+        if (citations.at(h) >= citations.size() - h) return h;
 
-    return h_index;
+    return -1;
 }

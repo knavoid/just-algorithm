195c268339b3b48c34180bf5d74e233f1e9f0653
knavoid <k_shun@naver.com>
2024-02-05 19:29:37 +0900

    [SWEA] 1868 파핑파핑 지뢰찾기

--- sw-expert-academy/D4/파핑파핑 지뢰찾기.cpp
+++ sw-expert-academy/D4/파핑파핑 지뢰찾기.cpp
@@ -4,88 +4,81 @@
 
 using namespace std;
 
-int T;
-int N; // 지뢰 찾기 표 크기
-string landmine[300]; // 지뢰 정보
+int T, N, click;
+string map[300];
+int bombCnt[300][300];
+bool visit[300][300];
 int direct[8][2] = { {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1} };
-int answer; // 최소 클릭 횟수
-
-bool inRange(int x, int y) {
-	return 0 <= x && x < N && 0 <= y && y < N;
-}
-
-// 해당 칸의 주변 지뢰 개수 반환
-int landmineCount(int x, int y) {
-	int count = 0;
-	for (int d = 0; d < 8; d++) {
-		int dx = x + direct[d][0];
-		int dy = y + direct[d][1];
-		if (inRange(dx, dy) && landmine[dx][dy] == '*') {
-			count++;
-		}
-	}
-	return count;
-}
-
-void click(int x, int y) {
-	queue<pair<int, int>> q;
-	q.push(make_pair(x, y));
-	landmine[x][y] = '0';
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		for (int d = 0; d < 8; d++) {
-			int dx = x + direct[d][0];
-			int dy = y + direct[d][1];
-			if (inRange(dx, dy) && landmine[dx][dy] == '.') {
-				int count = landmineCount(dx, dy);
-				landmine[dx][dy] = count + '0';
-				if (count == 0) {
-					q.push(make_pair(dx, dy));
-				}
-			}
-		}
-	}
-	answer++;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
 	cin >> T;
 	for (int tc = 1; tc <= T; tc++) {
-		// initialize
-		answer = 0;
 		cin >> N;
+		click = 0;
 		for (int i = 0; i < N; i++) {
-			cin >> landmine[i];
+			cin >> map[i];
 		}
-
-		// 0을 찾아서 클릭
+		fill(bombCnt[0], bombCnt[N - 1] + N, 0);
+		fill(visit[0], visit[N - 1] + N, 0);
+		// 지뢰 수 기록
 		for (int x = 0; x < N; x++) {
+
 			for (int y = 0; y < N; y++) {
-				if (landmine[x][y] == '.' && landmineCount(x, y) == 0) {
-					click(x, y);
+				if (map[x][y] != '*') {
+					for (int d = 0; d < 8; d++) {
+						int dx = x + direct[d][0];
+						int dy = y + direct[d][1];
+						if (0 <= dx && dx < N && 0 <= dy && dy < N && map[dx][dy] == '*') {
+							bombCnt[x][y]++;
+						}
+					}
+				}
+				else {
+					bombCnt[x][y] = -1;
 				}
 			}
 		}
-
-		// 0이 아닌 다른 수 클릭
+		// 0 클릭하기
 		for (int x = 0; x < N; x++) {
 			for (int y = 0; y < N; y++) {
-				if (landmine[x][y] == '.') {
-					answer++;
+				if (!visit[x][y] && bombCnt[x][y] == 0) {
+					click++;
+					queue<pair<int, int>> q;
+					q.push({ x, y });
+					visit[x][y] = true;
+					while (!q.empty()) {
+						int qx = q.front().first;
+						int qy = q.front().second;
+						q.pop();
+						for (int d = 0; d < 8; d++) {
+							int dx = qx + direct[d][0];
+							int dy = qy + direct[d][1];
+							if (0 <= dx && dx < N && 0 <= dy && dy < N && !visit[dx][dy]) {
+								visit[dx][dy] = true;
+								if (bombCnt[dx][dy] == 0) {
+									q.push({ dx, dy });
+								}
+							}
+						}
+					}
 				}
 			}
 		}
-
-		// result
-		cout << '#' << tc << ' ' << answer << '\n';
-	}
+		// 0이 아닌 수 클릭하기
+		for (int x = 0; x < N; x++) {
+			for (int y = 0; y < N; y++) {
+				if (!visit[x][y] && 0 < bombCnt[x][y]) {
+					click++;
+				}
+			}
+		}
 	
+		cout << '#' << tc << ' ' << click << '\n';
+	}
 
 	return 0;
 }
